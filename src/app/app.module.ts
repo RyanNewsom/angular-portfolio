527948566e39be0ef77f46d5df4e8bc1e5059af9
Ryan Newsom <Ryan.J.Newsom@kp.org>
2017-03-25 17:16:24 -0600

    Added mock web api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule}   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
 
 import { BlogpostsComponent }  from './blog/blogposts.component';
 import { AppComponent }  from './app.component';
@@ -8,10 +10,16 @@ import {BlogDetailComponent} from "./blog/blog-detail.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import { AppRoutingModule }     from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
   imports:      [ BrowserModule,
                   FormsModule,
-                  AppRoutingModule
+                  AppRoutingModule,
+                  HttpModule,
+                  InMemoryWebApiModule.forRoot(InMemoryDataService),
                 ],
 
   declarations: [ AppComponent, BlogDetailComponent, BlogpostsComponent, DashboardComponent ], //view classes that belong to this (Components, directives, pipes)
--- src/app/blog/blog.service.ts
+++ src/app/blog/blog.service.ts
@@ -1,24 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 import {BlogPost} from "./BlogPost";
-import {BLOG_POSTS} from "./mock-blogposts";
 
 @Injectable()
 export class BlogService {
 
+  private blogsUrl = 'api/blogPosts';  // URL to web api
+
+  constructor(private http: Http) { }
 
   getBlogPosts(): Promise<BlogPost[]> {
-    return Promise.resolve(BLOG_POSTS);
+    return this.http.get(this.blogsUrl)
+      .toPromise()
+      .then(response => response.json().data as BlogPost[])
+      .catch(this.handleError);
   }
 
   getBlogPostsSlowly(): Promise<BlogPost[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(this.getBlogPosts()), 2000);
-    });
+    return this.http.get(this.blogsUrl)
+      .toPromise()
+      .then(response => response.json().data as BlogPost[])
+      .catch(this.handleError);
   }
 
   getBlogPostById(id: number): Promise<BlogPost> {
-    return this.getBlogPosts().then(blogPosts => blogPosts.find(blogPost => blogPost.id == id));
+    const url = `${this.blogsUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as BlogPost)
+      .catch(this.handleError);  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 
 }
