8e729609cd08b722a37bd379bf80c3734b24323c
Ryan Newsom <Ryan.J.Newsom@kp.org>
2017-03-25 14:58:44 -0600

    basic routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,97 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import {BlogPost} from "./blog/BlogPost";
-import {BlogService} from "./blog/blog.service"
-
-//controls a patch of screen(presenter)
-
-
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  providers: [BlogService],
   template: `
-     <h1>{{title}}</h1>
-     
-     <h2>My Blog Posts</h2>
-    <ul class="blogPosts">
-      
-      <li *ngFor="let post of blogPosts" 
-      [class.selected]="post === selectedPost"
-      (click)="onSelect(post)">
-        <span class="badge">{{post.id}}</span> {{post.name}}
-      </li>
-    </ul>  
-    
-    <blog-detail [post]="selectedPost"></blog-detail>
-    `,
-  styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .blogPosts {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .blogPosts li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .blogPosts li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .blogPosts li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .blogPosts .text {
-      position: relative;
-      top: -3px;
-    }
-    .blogPosts .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `]
+    <h1>{{title}}</h1>
+    <a routerLink="/blog">Blog Posts</a>
+   <router-outlet></router-outlet>
+  `
 })
 
-export class AppComponent  implements OnInit {
-  constructor(private blogPostService: BlogService){ }
-
-  title = 'Blog';
-  blogPosts:  BlogPost[];
-  selectedPost: BlogPost;
-
-  ngOnInit(): void {
-    this.getBlogPosts();
-  }
-
-  onSelect(post: BlogPost): void {
-    this.selectedPost = post;
-  }
-
-  getBlogPosts(): void {
-    this.blogPostService.getBlogPostsSlowly().then(blogPosts => this.blogPosts = blogPosts);
-  }
+export class AppComponent {
+  title = 'Tour of Heroes';
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule}   from '@angular/forms';
 
+import { BlogpostsComponent }  from './blogposts.component';
 import { AppComponent }  from './app.component';
 import {BlogDetailComponent} from "./blog/blog-detail.component";
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
   imports:      [ BrowserModule,
-                  FormsModule
+                  FormsModule,
+                  RouterModule.forRoot([
+                    {
+                      path: 'blog',
+                      component: BlogpostsComponent
+                    }
+                  ])
                 ],
-  declarations: [ AppComponent, BlogDetailComponent ], //view classes that belong to this (Components, directives, pipes)
+
+  declarations: [ AppComponent, BlogDetailComponent, BlogpostsComponent ], //view classes that belong to this (Components, directives, pipes)
   bootstrap:    [ AppComponent ] //root application view, only declared in the main module
 })
 export class AppModule { }
