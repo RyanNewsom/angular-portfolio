90c4e5a635cf3a974b896a2327fe9eff878bee1d
Ryan Newsom <Ryan.J.Newsom@kp.org>
2017-03-25 15:03:58 -0600

    Added a dashboard/home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <a routerLink="/blog">Blog Posts</a>
+   <h1>{{title}}</h1>
+   <nav>
+     <a routerLink="/home">Home</a>
+     <a routerLink="/blog">Blog Posts</a>
+   </nav>
    <router-outlet></router-outlet>
-  `
+ `
+
 })
 
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BlogpostsComponent }  from './blog/blogposts.component';
 import { AppComponent }  from './app.component';
 import {BlogDetailComponent} from "./blog/blog-detail.component";
 import { RouterModule }   from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 @NgModule({
   imports:      [ BrowserModule,
@@ -14,11 +15,20 @@ import { RouterModule }   from '@angular/router';
                     {
                       path: 'blog',
                       component: BlogpostsComponent
+                    },
+                    {
+                      path: 'home',
+                      component: DashboardComponent
+                    },
+                    {
+                      path: '',
+                      redirectTo: '/home',
+                      pathMatch: 'full'
                     }
                   ])
                 ],
 
-  declarations: [ AppComponent, BlogDetailComponent, BlogpostsComponent ], //view classes that belong to this (Components, directives, pipes)
+  declarations: [ AppComponent, BlogDetailComponent, BlogpostsComponent, DashboardComponent ], //view classes that belong to this (Components, directives, pipes)
   bootstrap:    [ AppComponent ] //root application view, only declared in the main module
 })
 export class AppModule { }
